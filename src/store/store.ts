491b51f443ed7ce88a9a2c34d761666eb3200aab
Cristina <cristinajmartin98@gmail.com>
2023-02-22 20:45:44 +0100

    Add pokemonsSlice with first reducer loadPokemons case

---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { pokemonsReducer } from "./features/pokemonsSlice/pokemonsSlice";
+
+export const store = configureStore({
+  reducer: { pokemons: pokemonsReducer },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
