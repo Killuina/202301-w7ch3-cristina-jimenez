71a03f016a3952c0f950b7d50d53acea929d7ba1
Cristina <cristinajmartin98@gmail.com>
2023-02-22 21:23:28 +0100

    Add pokemon card

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { PokemonStructure } from "../../types";
+import PokemonCardStyled from "./PokemonCardStyled";
+
+interface PokemonCardProps {
+  pokemon: PokemonStructure;
+}
+
+const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
+  return (
+    <PokemonCardStyled className="pokemon-card">
+      <h2 className="pokemon-card__name">{pokemon.name}</h2>
+      <img
+        className="pokemon-card__image"
+        src={pokemon.image}
+        alt={pokemon.name}
+      />
+    </PokemonCardStyled>
+  );
+};
+
+export default PokemonCard;
